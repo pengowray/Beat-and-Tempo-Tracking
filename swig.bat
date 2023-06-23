94ef8e8f380e5f19db8dbacf72e4e7f5c4e296b3
Pengo Wray <member@pengo.org>
2023-06-23 13:16:57 +1000

    swig support

--- swig.bat
+++ swig.bat
@@ -1,8 +1,19 @@
 @echo off
 setlocal
 set PATH=%PATH%;C:\apps\swigwin-4.1.1
+set PATH=%PATH%;C:\msys64\mingw64\bin
+set PATH=%PATH%;C:\msys64
 
-mkdir bttswig
-swig.exe -csharp -outdir bttswig BTT.i 
+mkdir btt-cs
+swig.exe -csharp -outdir btt-cs BTT.i 
+
+rem gcc -lm -lpthread -lrt
+
+mkdir build
+cd build
+gcc -c -fpic -lm ../src/*.c ../BTT_wrap.c 
+gcc -shared *.o -o BTT.dll
+rem mono-csc -out:runme.exe *.cs
+rem cat runme.cs
 
 endlocal
